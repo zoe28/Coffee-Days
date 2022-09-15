bf811a232f1b6a172e364389eaa9e36a8d2f5b38
Zoe <32535293+zoe28@users.noreply.github.com>
2022-09-14 23:21:09 -0700

    api calls to get user

--- web/pages/profile.js
+++ web/pages/profile.js
@@ -1,65 +1,79 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import TopNavbar from "../components/TopNavbar"
 import { Form, Icon, Block, Button, Container } from 'react-bulma-components';
 
-
 import 'bulma/css/bulma.min.css';
 
+
 const Profile = () => {
-  const [username, setUsername] = useState('');
+  
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('')
   const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
-  const [message, setMessage] = useState('');
-  const [tocAgreed, setTocAgreed] = useState(false);
-  const [questionValue, setQuestionValue] = useState('');
+
+
+  useEffect(() => {
+    fetch('/api/user/first')
+      .then((res) => res.json())
+      .then((jsonData) => {
+        console.log(jsonData)
+        // const {first_name, last_name, email} = jsonData; // destructuring
+        setFirstName(jsonData.first_name)
+        setLastName(jsonData.last_name)
+        setEmail(jsonData.email)
+      })
+  }, []) // run this once when the component is first rendered
+
+  /* TODO:
+  1. add a click handler function
+  2. inside the function, use fetch to make a POST request
+  3. pass in firstName, lastName, email as part of the body
+  */
 
   return (
     <Container>
-    <TopNavbar />
-    
-    <form>
-      <Form.Field>
-        <Form.Label>First name</Form.Label>
-          <Form.Input
-            value={username}
-            onChange={(e) => {
-              return setUsername(e.target.value);
-            }}
-          />
-      </Form.Field>
+      <TopNavbar />
+      <form>
+        <Form.Field>
+          <Form.Label>First name</Form.Label>
+            <Form.Input
+              value={firstName}
+              onChange={(e) => {
+                return setFirstName(e.target.value);
+              }}
+            />
+        </Form.Field>
 
+        <Form.Field>
+          <Form.Label>Last name</Form.Label>
+            <Form.Input
+              value={lastName}
+              onChange={(e) => {
+                return setLastName(e.target.value);
+              }}
+            />
+        </Form.Field>
 
-      <Form.Field>
-        <Form.Label>Last name</Form.Label>
-          <Form.Input
-            value={username}
-            onChange={(e) => {
-              return setUsername(e.target.value);
-            }}
-          />
-      </Form.Field>
+        <Form.Field>
+          <Form.Label>Email</Form.Label>
+            <Form.Input
+              value={email}
+              onChange={(e) => {
+                return setEmail(e.target.value);
+              }}
+            />
+        </Form.Field>
 
-      <Form.Field>
-        <Form.Label>Email</Form.Label>
-          <Form.Input
-            value={email}
-            onChange={(e) => {
-              return setEmail(e.target.value);
-            }}
-          />
-      </Form.Field>
-
-      <Form.Field kind="group">
-        <Form.Control>
-          <Button color="warning">Submit</Button>
-        </Form.Control>
-        <Form.Control>
-          <Button color="link" colorVariant="light">
-            Cancel
-          </Button>
-        </Form.Control>
-      </Form.Field>
-    </form>
+        <Form.Field kind="group">
+          {/*
+          TODO:
+              1. call the click handler to make the POST request
+          */}
+          <Form.Control>
+            <Button color="warning">Update</Button>
+          </Form.Control>
+        </Form.Field>
+      </form>
     </Container>
   );  
 };
