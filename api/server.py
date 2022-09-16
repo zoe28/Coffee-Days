39d3205adfe18f415a5306177394bdab99a24a77
Zoe <32535293+zoe28@users.noreply.github.com>
2022-09-15 23:40:33 -0700

    add API get endpoints

--- api/server.py
+++ api/server.py
@@ -6,21 +6,34 @@ from models import connect_to_db, db, User, Shop, Review
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
     return 'flask server'
 
+
+def jsonify_user(user):
+    return jsonify({
+        'first_name': user.first_name,
+        'last_name': user.last_name,
+        'email': user.email,
+    })
+
+
+@app.route("/api/user/first")
+def get_first_user():
+    first_user = User.query.first()
+    return jsonify_user(first_user)
+
+
+# GET a user
 @app.route("/api/user/<user_id>")
 def get_user(user_id):
     """ Return a data on that user"""
 
     user = User.query.get(user_id)
     print(user)
-    return jsonify({
-        'first_name': user.first_name,
-        'last_name': user.last_name,
-        'email': user.email,
-    })
+    return jsonify_user(user)
 
 
 if __name__ == "__main__":
