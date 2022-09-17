58af9919134b5e7c3b3fc3cc1365a25021c196d0
Zoe <32535293+zoe28@users.noreply.github.com>
2022-09-17 14:32:23 -0700

    updated navbar styling

--- web/components/TopNavbar.js
+++ web/components/TopNavbar.js
@@ -7,31 +7,32 @@ import 'bulma/css/bulma.min.css';
 
 const TopNavbar = () => {
   return (
-    <Container>
-    <Navbar>
-      <Navbar.Brand>
-        <Navbar.Item href="/">
-          <Image
-            alt="Coffee Days Logo"
-            src="/coffee-days-logo.png"
-            width="216"
-            height="216"
-          /> 
-          Coffee Days
-        </Navbar.Item>
-        <Navbar.Burger />
-      </Navbar.Brand>
-      <Navbar.Menu>
-        <Navbar.Container>
-        </Navbar.Container>
-        <Navbar.Container align="end">
-          <Navbar.Item href="/profile">
-           Profile
+    <Navbar className="has-shadow">
+      <Container>
+        <Navbar.Brand>
+          <Navbar.Item href="/">
+            <Image
+              alt="Coffee Days Logo"
+              src="/coffee-days-logo.png"
+              width="216"
+              height="216"
+            /> 
+            <span className="title is-4 ml-1">Coffee Days</span>
           </Navbar.Item>
-        </Navbar.Container>
-      </Navbar.Menu>
+          <Navbar.Burger />
+        </Navbar.Brand>
+        <Navbar.Menu>
+          <Navbar.Container className="navbar-end" align="end">
+            <Navbar.Item href="/reviews">
+              Reviews
+            </Navbar.Item>
+            <Navbar.Item href="/profile">
+              Profile
+            </Navbar.Item>
+          </Navbar.Container>
+        </Navbar.Menu>
+      </Container>
     </Navbar>
-    </Container>
   );
 
 };
