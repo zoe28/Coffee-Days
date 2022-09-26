3900008ef1c30eacdea1f721bd7eb315fc68653e
Zoe <32535293+zoe28@users.noreply.github.com>
2022-09-25 23:00:33 -0700

    global the map

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -1,13 +1,23 @@
-import TopNavbar from "../components/TopNavbar"
+import TopNavbar from "../components/TopNavbar";
+import { useLoadScript } from "@react-google-maps/api";
 
+import "bulma/css/bulma.min.css";
 
 function MyApp({ Component, pageProps }) {
+  const { isLoaded } = useLoadScript({
+    libraries: ["places"],
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+  });
+
+  // show "Loading..." while GMaps JS is loading
+  if (!isLoaded) return <div>Loading...</div>;
+
   return (
     <>
       <TopNavbar />
       <Component {...pageProps} />
     </>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
