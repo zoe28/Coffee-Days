c4047c256581998121a90851d35dd3b2bb2bc1a7
Zoe <32535293+zoe28@users.noreply.github.com>
2022-09-25 23:01:58 -0700

    add some css styles

--- web/components/TopNavbar.js
+++ web/components/TopNavbar.js
@@ -1,13 +1,13 @@
-import Link from "next/link"
-import Image from 'next/image'
-import { Container, Navbar } from 'react-bulma-components';
+import Link from "next/link";
+import Image from "next/image";
+import { Container, Navbar } from "react-bulma-components";
 
-
-import 'bulma/css/bulma.min.css';
+import styles from "../styles/navbar.module.css";
 
 const TopNavbar = () => {
+  const navbarClassName = `${styles.topNavbar} has-shadow`;
   return (
-    <Navbar className="has-shadow">
+    <Navbar className={navbarClassName}>
       <Container>
         <Navbar.Brand>
           <Navbar.Item href="/">
@@ -16,28 +16,21 @@ const TopNavbar = () => {
               src="/coffee-days-logo.png"
               width="216"
               height="216"
-            /> 
+            />
             <span className="title is-4 ml-1">Coffee Days</span>
           </Navbar.Item>
           <Navbar.Burger />
         </Navbar.Brand>
         <Navbar.Menu>
           <Navbar.Container className="navbar-end" align="end">
-            <Navbar.Item href="/reviews">
-              Reviews
-            </Navbar.Item>
-            <Navbar.Item href="/profile">
-              Profile
-            </Navbar.Item>
-            <Navbar.Item href="/sign-up">
-              Sign Up
-            </Navbar.Item>
+            <Navbar.Item href="/reviews">Reviews</Navbar.Item>
+            <Navbar.Item href="/profile">Profile</Navbar.Item>
+            <Navbar.Item href="/sign-up">Sign Up</Navbar.Item>
           </Navbar.Container>
         </Navbar.Menu>
       </Container>
     </Navbar>
   );
-
 };
 
 export default TopNavbar;
