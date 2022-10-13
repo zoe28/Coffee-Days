a6b28f53155bf0a5d98f269f74ee793e65e71435
Zoe <32535293+zoe28@users.noreply.github.com>
2022-10-12 22:15:17 -0700

    fix user_id bug

--- api/models.py
+++ api/models.py
@@ -91,41 +91,47 @@ def add_example_data():
     review1 = Review(
         rating_score = 5,
         comment = "This shop serve great espresso, which is well prepared and on par with other specialty shops, but what really makes them stand out are the coffee creations they serve.",
-        user_id = leonard.id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1"
+        user_id = leonard.user_id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1"
         shop_id = blue_bottle_coffee.shop_id
     )
 
     review2 = Review(
         rating_score = 5,
         comment = "This is my favourite coffee shop in town.  The espresso is so balanced.  I would describe it as a very sophisticated flavour, you can clearly distinguish several layers of taste.",
-        user_id = leonard.id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
+        user_id = leonard.user_id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
         shop_id = andytown_coffee_roasters.shop_id
     )
 
     review3 = Review(
         rating_score = 5,
         comment = "This shop is absolutely a (hidden) gem! Easily one of my top two coffee shops in the area and I have been to a few dozen.",
-        user_id = Happy.id, # "5c23b423-87f7-42aa-a176-ced2159da035",
+        user_id = liz.user_id, # "5c23b423-87f7-42aa-a176-ced2159da035",
         shop_id = coffee_movement.shop_id
     )
 
     review4 = Review(
         rating_score = 3,
         comment = "Coffee is good but has a long wait",
-        user_id = leonard.id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
+        user_id = leonard.user_id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
         shop_id = blue_bottle_coffee.shop_id
     )
 
     review5 = Review(
         rating_score = 1,
         comment = "This shop does not have customer restroom or wifi. Music too loud and super slow service!",
-        user_id = leonard.id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
+        user_id = leonard.user_id, # "fc06e892-6c5a-42e4-ac53-d4842e4936e1",
         shop_id = ritual_coffee_roasters.shop_id
     )
 
 
     db.session.add_all([
         leonard, liz, peter, howl, ])
+
+    db.session.add_all([
+        blue_bottle_coffee, ritual_coffee_roasters, andytown_coffee_roasters, philz_coffee, coffee_movement])
+
+    db.session.add_all([
+        review1, review2, review3, review4, review5])
     db.session.commit()
 
 
