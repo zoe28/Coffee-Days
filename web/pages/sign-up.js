d71887817698c941a82d8056892163cc8bba505a
Zoe <32535293+zoe28@users.noreply.github.com>
2022-10-16 23:16:27 -0700

    add log in authentication

--- web/pages/sign-up.js
+++ web/pages/sign-up.js
@@ -1,15 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import { Form, Button, Container, Heading } from 'react-bulma-components';
+import React, { useEffect, useState } from "react";
+import { Form, Button, Container, Heading } from "react-bulma-components";
 
-import 'bulma/css/bulma.min.css';
-
+import "bulma/css/bulma.min.css";
+import { useRouter } from "next/router";
 
 const SignUp = () => {
-
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('')
-  const [email, setEmail] = useState('');
-
+  const router = useRouter();
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleClick = (evt) => {
     evt.preventDefault();
@@ -18,27 +18,29 @@ const SignUp = () => {
       first_name: firstName,
       last_name: lastName,
       email: email,
+      password: password,
     };
 
-    fetch('/api/user/create', {
-      method: 'POST',
+    fetch("/api/user/create", {
+      method: "POST",
       body: JSON.stringify(body),
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     })
-    .then((response) => {
-      if (response.status === 200) {
-        alert("User created!")
-      }
-      return response.json()
-    })
-    .then((data) => {
-      console.log(data)
-    })
+      .then((response) => {
+        if (response.status === 200) {
+          alert("User created!");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        localStorage.setItem("user_id", data.id);
+        router.push({
+          pathname: "/profile",
+        });
+      });
   };
-
-
 
   return (
     <Container>
@@ -46,45 +48,53 @@ const SignUp = () => {
       <form>
         <Form.Field>
           <Form.Label>First name</Form.Label>
-            <Form.Input
-              value={firstName}
-              onChange={(e) => {
-                return setFirstName(e.target.value);
-              }}
-            />
+          <Form.Input
+            value={firstName}
+            onChange={(e) => {
+              return setFirstName(e.target.value);
+            }}
+          />
         </Form.Field>
 
         <Form.Field>
           <Form.Label>Last name</Form.Label>
-            <Form.Input
-              value={lastName}
-              onChange={(e) => {
-                return setLastName(e.target.value);
-              }}
-            />
+          <Form.Input
+            value={lastName}
+            onChange={(e) => {
+              return setLastName(e.target.value);
+            }}
+          />
         </Form.Field>
 
         <Form.Field>
           <Form.Label>Email</Form.Label>
-            <Form.Input
-              value={email}
-              onChange={(e) => {
-                return setEmail(e.target.value);
-              }}
-            />
+          <Form.Input
+            value={email}
+            onChange={(e) => {
+              return setEmail(e.target.value);
+            }}
+          />
         </Form.Field>
 
-        
-        <Button
-            color="warning"
-            onClick={(evt) => {
-              return handleClick(evt);
+        <Form.Field>
+          <Form.Label>Password</Form.Label>
+          <Form.Input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              return setPassword(e.target.value);
             }}
-          >
-            Sign Up
-        </Button>
-       
+          />
+        </Form.Field>
 
+        <Button
+          color="warning"
+          onClick={(evt) => {
+            return handleClick(evt);
+          }}
+        >
+          Sign Up
+        </Button>
       </form>
     </Container>
   );
