5d3dc53024bdcc131cc328041ecc3bff09f8cc21
Zoe <32535293+zoe28@users.noreply.github.com>
2023-02-20 16:27:57 -0800

    update api routing for heroku production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 module.exports = {
   async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://127.0.0.1:5000/api/:path*',
-      },
-    ]
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://127.0.0.1:5000/api/:path*",
+        },
+      ];
+    } else if (process.env.DOMAIN) {
+      return [
+        {
+          source: "/api/:path*",
+          destination: `${process.env.DOMAIN}:8000/api/:path*`,
+        },
+      ];
+    }
   },
 };
