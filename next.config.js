e00a29560523d53b17d7cc11338e9ba3f4b52753
Zoe <32535293+zoe28@users.noreply.github.com>
2023-02-23 08:17:37 -0800

    remove next.js url redirect

--- next.config.js
+++ next.config.js
@@ -1,19 +1,10 @@
 module.exports = {
   async rewrites() {
-    if (process.env.NODE_ENV === "development") {
-      return [
-        {
-          source: "/api/:path*",
-          destination: "http://127.0.0.1:5000/api/:path*",
-        },
-      ];
-    } else if (process.env.DOMAIN) {
-      return [
-        {
-          source: "/api/:path*",
-          destination: `${process.env.DOMAIN}:8000/api/:path*`,
-        },
-      ];
-    }
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://127.0.0.1:5000/api/:path*",
+      },
+    ];
   },
 };
